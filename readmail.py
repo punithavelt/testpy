49efa5094f7e41978ba46ebb7cf6a9e7afe0c1a5
punithavelt <161704471+punithavelt@users.noreply.github.com>
2024-03-19 12:50:16 +0530

    Update readmail.py

--- readmail.py
+++ readmail.py
@@ -1,3 +1,4 @@
+
 import imaplib
 from email.header import decode_header
 
@@ -6,7 +7,12 @@ EMAIL = 'tech@thestagings.com'
 PASSWORD = 'v52V9f4cD'
 IMAP_SERVER = 'imap.marissa.metanet.ch'
 
-SPECIFIC_SUBJECT = 'Approval Granted test-pipeline'
+SPECIFIC_SUBJECT = ''
+
+def validate_email(subject, receiver_email):
+    # Your validation logic here
+    #print(f"Validating email with subject: {subject} and receiver email: {receiver_email}")
+    
 
 def get_subjects():
     # Connect to the mail server
@@ -16,30 +22,44 @@ def get_subjects():
 
     # Search for emails with a specific subject
     #result, data = mail.search(None, '(SUBJECT "test mail")')
-    result, data = mail.search(None, '(UNSEEN SUBJECT "{}")'.format(SPECIFIC_SUBJECT))
+
+    search_criteria = '(FROM "{}" SUBJECT "{}")'.format(SPECIFIC_RECEIVER, SPECIFIC_SUBJECT)
+    result, data = mail.search(None, '(UNSEEN SUBJECT "{}")'.format(search_criteria))
     subjects = []
 
     if result == 'OK':
         for num in data[0].split():
             result, raw_email = mail.fetch(num, '(RFC822)')
             if result == 'OK':
-                try:
-                    subject = decode_header(raw_email[0][1].decode('utf-8'))[0][0]
-                    if isinstance(subject, bytes):
-                        subject = subject.decode('utf-8')
-                    subjects.append(subject)
-                except Exception as e:
-                    print(f"Error decoding subject: {e}")
-
+                for num in messages[0].split():
+                    # Fetch the email with the specified ID
+                    _, data = mail.fetch(num, '(RFC822)')
+                    # Parse the email message
+                    msg = email.message_from_bytes(data[0][1])
+                    # Check if the email contains the specific text in the body
+                  #  if SPECIFIC_TEXT in msg.get_payload():
+                    try:
+                        subject = decode_header(raw_email[0][1].decode('utf-8'))[0][0]
+                        if isinstance(subject, bytes):
+                            subject = subject.decode('utf-8')
+                        subjects.append(subject)
+                    except Exception as e:
+                        print(f"Error decoding subject: {e}")
     mail.close()
     mail.logout()
     return subjects
 
  
 if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python validate_email.py <subject> <receiver_email>")
+        sys.exit(1)
+    SPECIFIC_SUBJECT = sys.argv[1]
+    SPECIFIC_RECEIVER = sys.argv[2]
     subjects = get_subjects()
+    validate_email(SPECIFIC_SUBJECT, SPECIFIC_RECEIVER,subjects)
     if subjects:
-      print('OK')
+      print(subjects)
        
     else:
         print('ERROR')
